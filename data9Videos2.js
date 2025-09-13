d0f5687fc39242089739873f0dfa1007162ba64a
CBench137 <constitutionalbench@gmail.com>
2025-09-13 12:18:19 +0545

    Update data9Videos2.js

--- data9Videos2.js
+++ data9Videos2.js
@@ -1,7 +1,7 @@
 // data9video2.js - Videos Dataset for dropdownset
 var data9Videos2 = `ID,Title,Description,shorturl,longurl
-001,Motivation for Exam2,Premanadaji2,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
-002,Ninda2,Premanadaji2,seevideo.mp4,Videos/premanandajimaharajonninda.mp4
+001,Motivation for Exam3,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
+002,Ninda3,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
 `;
 
 // Dataset metadata
@@ -12,3 +12,4 @@ var data9Videos2Info = {
     columns: 5,
     primaryKey: "ID"
 };
+
